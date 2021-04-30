3fe3cddef90bd443884683771ca4832dbbc3b3af
euguroglu <e.uguroglu@yahoo.com>
2021-04-30 21:21:51 +0300

    default and named argument

---
+++ src/part1Basics/DefaultArgs.scala
@@ -0,0 +1,19 @@
+package part1Basics
+
+object DefaultArgs extends App{
+
+  def trFact(n: Int, acc: Int = 1): Int =
+    if (n <=1 ) acc
+    else trFact(n-1, n*acc)
+
+  val fact10 = trFact(10)
+ //defining default values
+  def savePicture(format: String = "jpg", width: Int = 1920, height: Int = 1080): Unit = println("saving picture")
+  savePicture(width = 800)
+}
+
+/*
+We need to:
+1. Pass in every leading arfument
+2. Name the arguments
+ */
